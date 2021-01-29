8d727eefe5a91dc3f96f3ac185000f7d4ca77488
林海瑞 <lin.hairui@darchtech.com>
2021-01-29 16:58:49 +0800

    网格修改策略fix

--- core/bot/index.ts
+++ core/bot/index.ts
@@ -144,7 +144,11 @@ export default class TradeBot {
       return;
     }
     const gridPrice = lastOperatingPrice * Math.pow((1 + this.baseOptions.sellUpRate), i - 1);
-    lastGrid.nextBuyPrice = parseFloat((gridPrice * (1 - this.baseOptions.buyDownRate)).toFixed(2));
+    const nextBuyPrice = parseFloat((gridPrice * (1 - this.baseOptions.buyDownRate)).toFixed(2));
+    if (nextBuyPrice <= lastGrid.nextBuyPrice) {
+      return;
+    }
+    lastGrid.nextBuyPrice = nextBuyPrice;
     console.log('开始修改网格价格，当前价格为' + price + ', 上一次操作价格为 ' + lastOperatingPrice + ', 共完整跨越 ' + i + ' 个网格，修改后补仓价为：' + lastGrid.nextBuyPrice);
     this.dbHelper.updateOrAddGrid(lastGrid)
     this.dbHelper.flushIntoFile()
